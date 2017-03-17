b4dc214f8fa1556fdd04b9e10ca86ffa4f127363
孙兴隆 <1786141897@qq.com>
2017-03-17 15:48:16 +0800

    111

---
+++ app/scripts/controllers/list.js
@@ -0,0 +1,4 @@
+//angular.module('appApp', [])
+//.controller('list',['$scope','$http','$state',function($scope,$http,$state){
+//	
+//}])
---
+++ app/scripts/quan.js
@@ -0,0 +1,7 @@
+//window.onload =function(){
+	document.documentElement.style.fontSize = innerWidth / 64 + "px";
+			onresize = function() {
+				document.documentElement.style.fontSize = innerWidth / 64 + "px";
+			}
+//}
+			
